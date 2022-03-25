6eaba0637618bce4f433087a1508f97e83f806a4
breanner06 <92890666+breanner06@users.noreply.github.com>
2022-03-24 20:33:09 -0500

    update

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 let data = [
   {
-    name: 'Mateo',
-    age: '30'
+    name: 'Option 1',
+    price: '30'
   },
   {
-    name: 'Sarah',
-    age: '32'
+    name: 'Option 2',
+    price: '32'
   },
   {
-    name: 'John',
-    age: '20'
+    name: 'Option 3',
+    price: '20'
   },
   {
-    name: 'Tim',
-    age: '27'
+    name: 'Option 4',
+    price: '27'
   },
   {
-    name: 'Sam',
-    age: '22'
+    name: 'Option 5',
+    price: '22'
   },
   {
-    name: 'Joey',
-    age: '34'
+    name: 'Option 6',
+    price: '34'
   }
 ];
 
 const info = document.querySelector('#info');
 
 let details = data.map(function(item) {
-  return '<div>' + item.name + ' ' + 'is ' + item.age + ' years old' + '</div>';
+  return '<div>' + item.name + ' ' + 'is $' + item.price + '.00' + '</div>';
 });
 
 info.innerHTML = details.join('\n');
